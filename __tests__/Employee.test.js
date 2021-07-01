d6c310423b9a039c8ccf0db2123325af4866c6de
jlentz17 <64111084+jlentz17@users.noreply.github.com>
2021-07-01 16:34:12 -0700

    added and passed test for first method in employee

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -2,8 +2,15 @@ const { expect } = require("@jest/globals");
 const Employee = require("../lib/Employee");
 
 test("creates an employee object", () => {
-  const employee = new Employee("name", 500789, "email");
+  const employee = new Employee("Lars", 500789, "larsbars@yahoo.com");
+
   expect(employee.name).toEqual(expect.any(String));
   expect(employee.id).toEqual(expect.any(Number));
   expect(employee.email).toEqual(expect.any(String));
 });
+
+test("makes getName method to return employee's name", () => {
+  const employee = new Employee("Lars", 500789, "larsbars@yahoo.com");
+  expect(employee.getName()).toEqual(expect.any(String));
+
+})
--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,1 +1,16 @@
+const { expect } = require("@jest/globals");
 const Intern = require("../lib/Intern");
+
+test("creates an intern object", () => {
+  const intern = new Intern(
+    "Jessica",
+    84237984,
+    "jessica@gmail.com",
+    "cal state northridge"
+  );
+
+  expect(intern.name).toEqual(expect.any(String));
+  expect(intern.id).toEqual(expect.any(Number));
+  expect(intern.email).toEqual(expect.any(String));
+  expect(intern.school).toEqual(expect.any(String));
+});
