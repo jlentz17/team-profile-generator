cd2f0e2b83fbc0fe4e81dffb26a5588a15b6f50f
jlentz17 <64111084+jlentz17@users.noreply.github.com>
2021-07-01 15:44:10 -0700

    added and passed test for Engineer properties

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,1 +1,9 @@
 const Engineer = require("../lib/Engineer");
+
+test("creates an engineer object", () => {
+  const engineer = new Engineer("name", 500789, "email", 6137687134);
+  expect(engineer.name).toEqual(expect.any(String));
+  expect(engineer.id).toEqual(expect.any(Number));
+  expect(engineer.email).toEqual(expect.any(String));
+  expect(engineer.officeNumber).toEqual(expect.any(Number));
+});
