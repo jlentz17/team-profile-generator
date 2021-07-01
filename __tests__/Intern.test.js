185c62d39ea9bef4c3bd7524cf27e5cae3192467
jlentz17 <64111084+jlentz17@users.noreply.github.com>
2021-07-01 16:58:54 -0700

    added and passed test for Intern methods

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -14,3 +14,54 @@ test("creates an intern object", () => {
   expect(intern.email).toEqual(expect.any(String));
   expect(intern.school).toEqual(expect.any(String));
 });
+
+test("makes getName method to return intern's name", () => {
+    const intern = new Intern(
+        "Jessica",
+        84237984,
+        "jessica@gmail.com",
+        "cal state northridge"
+      );
+  
+    expect(intern.getName()).toEqual(expect.any(String));
+  });
+  
+  test("makes getId method to return intern's id", () => {
+    const intern = new Intern(
+        "Jessica",
+        84237984,
+        "jessica@gmail.com",
+        "cal state northridge"
+      );
+    expect(intern.getId()).toEqual(expect.any(Number));
+  });
+  
+  test("makes getEmail method to return intern's email", () => {
+    const intern = new Intern(
+        "Jessica",
+        84237984,
+        "jessica@gmail.com",
+        "cal state northridge"
+      );
+    expect(intern.getEmail()).toEqual(expect.any(String));
+  });
+  
+  test("makes getGithub method and returns intern's github info", () => {
+    const intern = new Intern(
+        "Jessica",
+        84237984,
+        "jessica@gmail.com",
+        "cal state northridge"
+      );
+    expect(intern.getSchool()).toEqual(expect.any(String));
+  });
+  
+  test("makes getRole method to return intern's role", () => {
+    const intern = new Intern(
+        "Jessica",
+        84237984,
+        "jessica@gmail.com",
+        "cal state northridge"
+      );
+    expect(intern.getRole()).toEqual(expect.any(String));
+  });
