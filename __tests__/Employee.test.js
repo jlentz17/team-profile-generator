3c714a186a78887454ee07f950dd64c320af1c6e
jlentz17 <64111084+jlentz17@users.noreply.github.com>
2021-07-01 16:49:54 -0700

    added and passed test for methods in engineer

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -12,5 +12,18 @@ test("creates an employee object", () => {
 test("makes getName method to return employee's name", () => {
   const employee = new Employee("Lars", 500789, "larsbars@yahoo.com");
   expect(employee.getName()).toEqual(expect.any(String));
+})
 
+test("makes getId method to return employee's id", () => {
+  const employee = new Employee("Lars", 500789, "larsbars@yahoo.com");
+  expect(employee.getId()).toEqual(expect.any(Number));
+})
+
+test("makes getEmail method to return employee's email", () => {
+  const employee = new Employee("Lars", 500789, "larsbars@yahoo.com");
+  expect(employee.getEmail()).toEqual(expect.any(String));
+})
+test("makes getRole method to return employee's role", () => {
+  const employee = new Employee("Lars", 500789, "larsbars@yahoo.com");
+  expect(employee.getRole()).toEqual(expect.any(String));
 })
--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,9 +1,37 @@
+const { expect } = require("@jest/globals");
 const Engineer = require("../lib/Engineer");
 
 test("creates an engineer object", () => {
   const engineer = new Engineer("name", 500789, "email", "github17@github.io");
+
   expect(engineer.name).toEqual(expect.any(String));
   expect(engineer.id).toEqual(expect.any(Number));
   expect(engineer.email).toEqual(expect.any(String));
   expect(engineer.github).toEqual(expect.any(String));
+});
+
+test("makes getName method to return engineer's name", () => {
+  const engineer = new Engineer("name", 500789, "email", "github17@github.io");
+
+  expect(engineer.getName()).toEqual(expect.any(String));
+});
+
+test("makes getId method to return engineer's id", () => {
+  const engineer = new Engineer("name", 500789, "email", "github17@github.io");
+  expect(engineer.getId()).toEqual(expect.any(Number));
+});
+
+test("makes getEmail method to return engineer's email", () => {
+  const engineer = new Engineer("name", 500789, "email", "github17@github.io");
+  expect(engineer.getEmail()).toEqual(expect.any(String));
+});
+
+test("makes getGithub method and returns engineer's github info", () => {
+  const engineer = new Engineer("name", 500789, "email", "github17@github.io");
+  expect(engineer.getGithub()).toEqual(expect.any(String));
+});
+
+test("makes getRole method to return engineer's role", () => {
+  const engineer = new Engineer("name", 500789, "email", "github17@github.io");
+  expect(engineer.getRole()).toEqual(expect.any(String));
 });
